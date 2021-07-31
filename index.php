2a1475af79f5ac0b3ac4135190bf04464526b500
Fernando Duarte Villavicencio <al222010329@gmail.com>
2021-07-31 09:04:45 -0500

    Añadimos el panel flotante en la parte de abajo de la pagina

--- index.php
+++ index.php
@@ -5,6 +5,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="shortcut icon" href="img/robot.svg" type="image/x-icon">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bulma@0.9.3/css/bulma.min.css">
     <link rel="stylesheet" href="/proyecto_integrador/css/bootstrap.min.css">
     <link rel="stylesheet" href="stile.css">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/css/foundation.min.css" integrity="sha256-ogmFxjqiTMnZhxCqVmcqTvjfe1Y/ec4WaRj/aQPvn+I=" crossorigin="anonymous">
@@ -51,6 +52,14 @@
       </div>
       <script  src="js/bootstrap.min.js"></script>
       <script src="https://cdn.jsdelivr.net/npm/foundation-sites@6.6.3/dist/js/foundation.min.js" integrity="sha256-pRF3zifJRA9jXGv++b06qwtSqX1byFQOLjqa2PTEb2o=" crossorigin="anonymous"></script>
+      <script src="js/eliminar.js"></script>
+<div class="notification">
+  <button class="delete"></button> 
+   <strong><a href="ingresa.php">Inicia sesion</a></strong>, Si aun no tienes cuenta puedes <strong><a href="registrate.php"> Registrarte gratuitamente</a> </strong> Gracias.
+</div>
+
 </body>
+
+
 
 </html>
