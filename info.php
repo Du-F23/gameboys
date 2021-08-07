c42291b759f8b37bd4fd147ba84836ebfbbfb576
Fernando Duarte Villavicencio <al222010329@gmail.com>
2021-08-06 21:41:24 -0500

    Añadimos la informacion en el apartado de info

--- info.php
+++ info.php
@@ -15,6 +15,15 @@
 <?php
     require_once './menu.php';
     ?>
+
+<a href="https://web.telegram.org"><img src="img/telegram.png" class="rounded float-start" alt="Telegram"  width="50" height="50">Telegram <p></p></a>
+<a href="https://www.instagram.com/"><img src="img/instagram.png" class="rounded float-end" alt="Instagram"width="50" height="50">Instagram <p></p></a>
+<a href="https://mail.google.com/"><img src="img/email.png" class="rounded float-start" alt="correo" width="50" height="50">Correo Electronico<p></p></a>
+
+<p>Tambien puedes encontrarnos en la siguiente ubicación</p>
+<center><iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3764.6607278846577!2d-99.47962168570984!3d19.340522986936683!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x85d20a14878a64eb%3A0x65bdb503fdce37bc!2sUniversidad%20Tecnol%C3%B3gica%20del%20Valle%20de%20Toluca!5e0!3m2!1ses-419!2smx!4v1628303924395!5m2!1ses-419!2smx" width="600" height="450" style="border:0;" allowfullscreen="" loading="lazy"></iframe></center>
+
+
 <div class="notification">
   <button class="delete"></button>
   Podemos asesorarte mandando un correo a 
